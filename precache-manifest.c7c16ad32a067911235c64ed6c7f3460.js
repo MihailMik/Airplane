ad7cf28c9643100f6ab2f2d5de4aa069932e9917
mik <m.miklyaev@yandex.ru>
2020-05-12 17:15:20 +0300

    first commit

---
+++ precache-manifest.c7c16ad32a067911235c64ed6c7f3460.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d39dbf1371b6493c289f677aba8a471",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a21a076cd7a32f3edf3e",
+    "url": "/static/css/main.5df0bc1d.chunk.css"
+  },
+  {
+    "revision": "5d1923ec8310f0733d60",
+    "url": "/static/js/2.44b235a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.44b235a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a21a076cd7a32f3edf3e",
+    "url": "/static/js/main.cb83b994.chunk.js"
+  },
+  {
+    "revision": "ed47fb97b705b9213ccf",
+    "url": "/static/js/runtime-main.ef888cc8.js"
+  }
+]);
