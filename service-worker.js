5f86445476000989accc80547f1ad110a964050c
mik <m.miklyaev@yandex.ru>
2020-05-13 12:12:39 +0300

    Version 3. Drinks are offered via Ask button only, not via Seat button

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2205822aaf1a118217cb07e8b261b9d0.js"
+  "/precache-manifest.61f159e16bfd61974746a05c14a40822.js"
 );
 
 self.addEventListener('message', (event) => {
