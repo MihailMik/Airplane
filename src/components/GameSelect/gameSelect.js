c9bb3bad2126fc6877e163fc72112c101679453e
mik <m.miklyaev@yandex.ru>
2020-06-15 16:32:40 +0300

    Version 43. Game 12x6 is default game now

--- src/components/GameSelect/gameSelect.js
+++ src/components/GameSelect/gameSelect.js
@@ -9,13 +9,13 @@ const GameSelect = (props) => {
         <div>
             <label className={s.paramName}>Game:
                 <select className={s.select} onChange={(e) => props.game.onChangeGameSelect(e.target.value)}>
-                    <option value={'9x6W123C1'} selected>9x6, 123W&1C in row</option>
+                    <option value={'9x6W123C1'}>9x6, 123W&1C in row</option>
                     <option value={'9x6W12C1'}>9x6, 12W&1C in row</option>
                     <option value={'9x5W123C1'}>9x5, 123W&1C in row</option>
                     {/*<option value={'6x4W12'}>6x4, 12W in row, 5C&10T in all</option>*/}
                     <option value={'6x4W1C1'}>6x4, 1W&1C in row</option>
                     <option value={'9x5W1C1'}>9x5, 1W&1C in row</option>
-                    <option value={'12x6W1C1'}>12x6, 1W&1C in row</option>
+                    <option value={'12x6W1C1'} selected>12x6, 1W&1C in row</option>
                     <option value={'param'}>arbitrary params</option>
                 </select>
             </label>
