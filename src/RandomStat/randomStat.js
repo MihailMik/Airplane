b455b8a66fdfea2667658964fcc4b09d0aeea4fe
mik <m.miklyaev@yandex.ru>
2020-06-23 14:34:47 +0300

    Version 58. Strategy3 added. Description added

--- src/RandomStat/randomStat.js
+++ src/RandomStat/randomStat.js
@@ -2,13 +2,134 @@ import React from "react"
 import s from './randomStat.module.css';
 import game from '../redux/store'
 
-const NPRIZES = 36 / 2 + 1;
 const T = 0
 const C = 1
 const W = 2
 const ONE_ROW = [W, C, T, T]
 const NROW = 6
+const PRIZE_TEA = 2
+const PRIZE_COFFEE = 4
 
+const makeDescription = (strings) => {
+    return strings.map((item, i) => <div key={i}>{item}</div>)
+}
+const description = {
+    '1T': [
+        'Q1: T IF T -> Q2: C',
+        'Q1: T IF C -> Q2: T',
+    ],
+    '1C': [
+        'Q1: C IF C -> Q2: T',
+        'Q1: C IF T -> Q2: C',
+    ],
+    'Strat2': [
+        'Q1: T IF T -> Q2: C IF (T&Prz<=c) -> Q3: C',
+        'else Next',
+        'Q1: T IF C -> Q2: T IF (T&Prz<=t) -> Q3: T',
+        'else Next',
+    ],
+    'Strat3': [
+        'Q1: T IF T -> Q2: T IF (T&Prz<=c) -> Q3: C',
+        'else Next',
+        'Q1: T IF C -> Q2: T IF (T&Prz<=t) -> Q3: T',
+        'else Next',
+    ],
+}
+const realization = {
+    '1T': [
+        'let prize = 0',
+        '    for (let row = 0; row < NROW; row++) {',
+        '    let seats = mix(ONE_ROW)',
+        '    const [one, two] = [...seats]',
+        '',
+        '    if (one === T) {',
+        '        prize += PRIZE_TEA',
+        '        if (two === C) prize += PRIZE_COFFEE',
+        '        else if (two === W) prize = 0',
+        '    } else if (one === C) {',
+        '        if (two === T) prize += PRIZE_TEA',
+        '        else if (two === W) prize = 0',
+        '    } else prize = 0',
+        '}',
+        'return prize',
+    ],
+    '1C': [
+        'let prize = 0',
+        'for (let row = 0; row < NROW; row++) {',
+        '    let seats = mix(ONE_ROW)',
+        '    const [one, two] = [...seats]',
+        '',
+        '    if (one === T) {',
+        '        prize += PRIZE_TEA',
+        '        if (two === C) prize += PRIZE_COFFEE',
+        '        else if (two === W) prize = 0',
+        '    } else if (one === C) {',
+        '        if (two === T) prize += PRIZE_TEA',
+        '        else if (two === W) prize = 0',
+        '    } else prize = 0',
+        '}',
+        'return prize'
+        ],
+    'Strat2': [
+        'let prize = 0',
+        'for (let row = 0; row < NROW; row++) {',
+        '    let seats = mix(ONE_ROW)',
+        '    const [one, two, three] = [...seats]',
+        '',
+        '    if (one === T) {',
+        '        prize += PRIZE_TEA',
+        '        if (two === C) prize += PRIZE_COFFEE',
+        '        else if (two === T) {',
+        '            prize += 0',
+        '            if (prize <= PRIZE_COFFEE) {',
+        '                if (three === C) prize += PRIZE_COFFEE',
+        '                else prize = 0',
+        '            }',
+        '        } else prize = 0',
+        '    } else if (one === C) {',
+        '        prize += 0',
+        '        if (two === T) {',
+        '            prize += PRIZE_TEA',
+        '            if (prize <= PRIZE_TEA) {',
+        '                if (three === T) prize += PRIZE_TEA',
+        '                else prize = 0',
+        '            }',
+        '        } else prize = 0',
+        '    } else prize = 0',
+        '}',
+        'return prize'
+    ],
+    'Strat3': [
+        'let prize = 0',
+        'for (let row = 0; row < NROW; row++) {',
+        '    let seats = mix(ONE_ROW)',
+        '    const [one, two, three] = [...seats]',
+        '',
+        '    if (one === T) {',
+        '        prize += PRIZE_TEA',
+        '        if (two === T) {',
+        '            prize += PRIZE_TEA',
+        '            if (prize <= PRIZE_COFFEE) {',
+        '                if (three === C) prize += PRIZE_COFFEE',
+        '                else prize = 0',
+        '            }',
+        '        } else if (two === C) {',
+        '            prize += 0',
+        '        } else prize = 0',
+        '    } else if (one === C) {',
+        '        prize += 0',
+        '        if (two === T) {',
+        '            prize += PRIZE_TEA',
+        '            if (prize <= PRIZE_TEA) {',
+        '                if (three === T) prize += PRIZE_TEA',
+        '                else prize = 0',
+        '            }',
+        '        } else prize = 0',
+        '    } else prize = 0',
+        '}',
+        'return prize'
+    ],
+}
 const mix = (arr) => {
     let nCol = arr.length
     let tmp = [...arr]
@@ -20,14 +141,19 @@ const mix = (arr) => {
     }
     return out
 }
-const PRIZE_TEA = 2
-const PRIZE_COFFEE = 4
+
 const PlayOne = () => {
     switch (stat.strategy) {
-        case '1T': return Strategy_1T ();
-        case '1C': return Strategy_1C ();
-        case 'Strat2': return Strategy_Strat2 ();
-        default: return 0;
+        case '1T':
+            return Strategy_1T();
+        case '1C':
+            return Strategy_1C();
+        case 'Strat2':
+            return Strategy_Strat2();
+        case 'Strat3':
+            return Strategy_Strat3();
+        default:
+            return 0;
     }
 }
 const Strategy_1T = () => {
@@ -42,9 +168,7 @@ const Strategy_1T = () => {
         } else if (one === C) {
             if (two === T) prize += PRIZE_TEA
             else if (two === W) prize = 0
-        } else {
-            prize = 0
-        }
+        } else prize = 0
     }
     return prize
 }
@@ -60,9 +184,7 @@ const Strategy_1C = () => {
         } else if (one === T) {
             if (two === C) prize += PRIZE_COFFEE
             else if (two === W) prize = 0
-        } else {
-            prize = 0
-        }
+        } else prize = 0
     }
     return prize
 }
@@ -82,8 +204,7 @@ const Strategy_Strat2 = () => {
                     if (three === C) prize += PRIZE_COFFEE
                     else prize = 0
                 }
-            }
-            else prize = 0
+            } else prize = 0
         } else if (one === C) {
             prize += 0
             if (two === T) {
@@ -92,11 +213,39 @@ const Strategy_Strat2 = () => {
                     if (three === T) prize += PRIZE_TEA
                     else prize = 0
                 }
-            }
-            else  prize = 0
-        } else {
-            prize = 0
-        }
+            } else prize = 0
+        } else prize = 0
+    }
+    return prize
+}
+
+const Strategy_Strat3 = () => {
+    let prize = 0
+    for (let row = 0; row < NROW; row++) {
+        let seats = mix(ONE_ROW)
+        const [one, two, three] = [...seats]
+
+        if (one === T) {
+            prize += PRIZE_TEA
+            if (two === T) {
+                prize += PRIZE_TEA
+                if (prize <= PRIZE_COFFEE) {
+                    if (three === C) prize += PRIZE_COFFEE
+                    else prize = 0
+                }
+            } else if (two === C) {
+                prize += 0
+            } else prize = 0
+        } else if (one === C) {
+            prize += 0
+            if (two === T) {
+                prize += PRIZE_TEA
+                if (prize <= PRIZE_TEA) {
+                    if (three === T) prize += PRIZE_TEA
+                    else prize = 0
+                }
+            } else prize = 0
+        } else prize = 0
     }
     return prize
 }
@@ -106,29 +255,30 @@ export let stat = {
     count: 0,
     gist: [],
     nInSeries: 100,
-    strategy: 'Strat2',
+    strategy: 'Strat3', NPRIZES: 38 / 2 + 1,
     timerId: undefined,
+    isDescription: false,
+    isRealization: false,
     stopTimer: () => {
         if (stat.timerId !== undefined)
             clearInterval(stat.timerId)
         stat.timerId = undefined
     },
     newStrategy: (strategy) => {
-      stat.clear ()
-      stat.strategy = strategy
+        stat.strategy = strategy;
+        stat.NPRIZES = (strategy === 'Strat3' ? 38 : 36) / 2 + 1;
+        stat.clear()
     },
     create: () => {
         stat.start = false
-        stat.count = 0
         stat.nInSeries = 100
-        for (let i = 0; i < NPRIZES; i++)
-            stat.gist[i] = 0
+        stat.clear();
         stat.stopTimer();
     },
 
     clear: () => {
         stat.count = 0
-        for (let i = 0; i < NPRIZES; i++)
+        for (let i = 0; i < stat.NPRIZES; i++)
             stat.gist[i] = 0
     },
 
@@ -147,36 +297,42 @@ export let stat = {
 }
 
 export default props => {
-    let totalPrzs = stat.gist.reduce((x,y,i)=>x+y*i*2, 0)
+    let totalPrzs = stat.gist.reduce((x, y, i) => x + y * i * 2, 0)
     // let totalPercent = stat.gist.reduce((x,y,i)=>x+y/(stat.count||1)*100, 0)
-    let average = totalPrzs/(stat.count || 1)
+    let average = totalPrzs / (stat.count || 1)
     const MakeAll = () => {
         const MakeRow = (row) => {
             const PX_FOR_ONE_GAME = 1
 
             //ищем змаксимальное значение в массиве без учета элемента с индексом 0
             let [, ...normaArr] = [...stat.gist]
-            const norma = Math.max (...normaArr)
+            const norma = Math.max(...normaArr)
 
-            let maxWidth = window.innerWidth - 480
+            let maxWidth = window.innerWidth - 390
             let coeff = PX_FOR_ONE_GAME
             if (norma * PX_FOR_ONE_GAME > maxWidth) {
                 coeff = (maxWidth - 20) / norma
             }
             let value = stat.gist[row]
-            let valuePrz = value*row*2
+            let valuePrz = value * row * 2
             let width = Math.min(maxWidth, value * coeff)
 
-            let percent = value/(stat.count||1)*100
-            let percentPrz = valuePrz/(totalPrzs||1)*100
+            let percent = value / (stat.count || 1) * 100
+            let percentPrz = valuePrz / (totalPrzs || 1) * 100
 
             return (
                 <div key={row}>
                     <button className={s.buttonPrize}>{row * 2}</button>
                     <button className={s.buttonGames}>{value}</button>
-                    <button className={s.buttonGames}>{percent.toLocaleString("ru-RU",{minimumFractionDigits:5, maximumFractionDigits:5})} %</button>
+                    <button className={s.buttonGames}>{percent.toLocaleString("ru-RU", {
+                        minimumFractionDigits: 3,
+                        maximumFractionDigits: 3
+                    })}</button>
                     <button className={s.buttonGames}>{valuePrz}</button>
-                    <button className={s.buttonGames}>{percentPrz.toLocaleString("ru-RU",{minimumFractionDigits:5, maximumFractionDigits:5})} %</button>
+                    <button className={s.buttonGames}>{percentPrz.toLocaleString("ru-RU", {
+                        minimumFractionDigits: 3,
+                        maximumFractionDigits: 3
+                    })}</button>
                     {width > 0 ?
                         <button className={s.buttonGist} style={{width: width}}/> : null
                     }
@@ -193,7 +349,7 @@ export default props => {
             <button className={s.butGames}>% Przs</button>
         </div>)
 
-        for (let row = 0; row < NPRIZES; row++) {
+        for (let row = 0; row < stat.NPRIZES; row++) {
             rows.push(MakeRow(row))
         }
         return rows;
@@ -206,15 +362,43 @@ export default props => {
             <div className={s.Select}>
                 <label>Strategy:
                     <select className={s.select} onChange={(e) => {
-                        stat.newStrategy (e.target.value)
+                        stat.newStrategy(e.target.value)
                         game.rerender()
                     }}>
-                        <option value={'1T'} selected={stat.strategy==='1T'}>1T</option>
-                        <option value={'1C'} selected={stat.strategy==='1C'}>1C</option>
-                        <option value={'Strat2'} selected={stat.strategy==='Strat2'}>Str2</option>
+                        <option value={'1T'} selected={stat.strategy === '1T'}>1T</option>
+                        <option value={'1C'} selected={stat.strategy === '1C'}>1C</option>
+                        <option value={'Strat2'} selected={stat.strategy === 'Strat2'}>Str2</option>
+                        <option value={'Strat3'} selected={stat.strategy === 'Strat3'}>Str3</option>
                     </select>
                 </label>
+
+                <button className={s.buttonDescription} onClick={() => {
+                    stat.isDescription = !stat.isDescription
+                    game.rerender()
+                }}>
+                    {stat.isDescription ? 'Скрыть описание' : 'Показать описание'}
+                </button>
+
+                <button className={s.buttonDescription} onClick={() => {
+                    stat.isRealization = !stat.isRealization
+                    game.rerender()
+                }}>
+                    {stat.isRealization ? 'Скрыть реализацию' : 'Показать реализацию'}
+                </button>
+
             </div>
+
+
+            {stat.isDescription ?
+                <div className={s.description} > {makeDescription (description[stat.strategy])} </div> :
+                null
+            }
+
+            {stat.isRealization ?
+                <div className={s.description} > {makeDescription (realization[stat.strategy])} </div> :
+                null
+            }
+
             <div>
                 <button className={s.toMainGame} onClick={() => {
                     props.game.prizeCoffeeStr = props.game.prizeTeaCoffeeStr * 4;
@@ -225,17 +409,22 @@ export default props => {
             </div>
 
             <div className={s.seria}>
-                <label>Длина серии (игр): </label>
+                <label>Игр в серии: </label>
                 <input className={s.edit} value={stat.nInSeries}
                        onChange={(e) => {
                            stat.nInSeries = Number(e.target.value);
                            props.game.rerender()
                        }}>
                 </input>
+                <button className={s.play} onClick={() => {
+                    stat.clear()
+                    props.game.rerender()
+                }}>Очистить статистику
+                </button>
             </div>
 
             <div>
-                <button className={s.playing} onClick={() => {
+                <button className={s.play} onClick={() => {
                     stat.stopTimer()
                     stat.start = !stat.start;
                     if (stat.start) {
@@ -257,32 +446,27 @@ export default props => {
             </div>
 
             <div>
-                <button className={s.clear} onClick={() => {
-                    stat.clear()
-                    props.game.rerender()
-                }}>Очистить статистику
-                </button>
-            </div>
-
-            <div>
                 {MakeAll()}
             </div>
 
-            <button className={s.butPrize} >Total:</button>
+            <button className={s.butPrize}>Total:</button>
             <button className={s.butGames}>{stat.count}</button>
-            <button className={s.butGames}>100 %</button>
+            <button className={s.butGames}>100</button>
             <button className={s.butGames}>{totalPrzs}</button>
-            <button className={s.butGames}>100 %</button>
+            <button className={s.butGames}>100</button>
 
 
             <div>
-            <span className={s.total}>Total Games: {stat.count.toLocaleString("ru-RU",{useGrouping:true})}</span>
+                <span className={s.total}>Total Games: {stat.count.toLocaleString("ru-RU", {useGrouping: true})}</span>
             </div>
             <div>
-                <span className={s.total}>Total Przes: {totalPrzs.toLocaleString("ru-RU",{useGrouping:true})}</span>
+                <span className={s.total}>Total Przes: {totalPrzs.toLocaleString("ru-RU", {useGrouping: true})}</span>
             </div>
             <div>
-                <span className={s.total}>Average Prz: {average.toLocaleString("ru-RU",{minimumFractionDigits:6, maximumFractionDigits:6})}</span>
+            <span className={s.total}>Average Prz: {average.toLocaleString("ru-RU", {
+                minimumFractionDigits: 6,
+                maximumFractionDigits: 6
+            })}</span>
             </div>
         </div>
     )
